34bff3636aa739faafe3143e35c2dd69ffd74670
KalibrateBlockchain <74208473+KalibrateBlockchain@users.noreply.github.com>
2021-05-05 17:21:49 -0400

    Update healthdrop_audio_processor.py

--- healthdrop_audio_processor.py
+++ healthdrop_audio_processor.py
@@ -47,7 +47,7 @@ def run_analysis_RITA(wav_path,wav_chunk, sampling_rate):
     # label = x['label'].max()
 
     # Save phasor plot and mel spectrogram for IAIF and estimated one
-    Sr, Sl = plot_phasor(wav_path, wav_chunk, results, "", g, sampling_rate)
+    Sr, Sl = plot_phasor(wav_path, wav_chunk, results['alpha'][-1], results['beta'][-1], results['delta'][-1], "", g, sampling_rate)
     # S.append([wav_file, label, Sr, Sl, results["alpha"][-1], results["beta"][-1], results["delta"][-1]])
     # plot_mel(wav_file, sample_rate, g, results['u0'], plot_dir_mel_spectrogram_true, plot_dir_mel_spectrogram_est, results)
 
