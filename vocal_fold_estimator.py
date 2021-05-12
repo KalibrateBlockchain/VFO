bd8444f819f74fc12ba7ad73253136e2903f48b9
KalibrateBlockchain <74208473+KalibrateBlockchain@users.noreply.github.com>
2021-05-11 22:11:07 -0400

    Update vocal_fold_estimator.py

--- vocal_fold_estimator.py
+++ vocal_fold_estimator.py
@@ -187,7 +187,7 @@ def vocal_fold_estimator(wav_file_path, wav_samples,sample_rate,glottal_flow, lo
             f"beta = {beta_k:.4f}   delta = {delta_k:.4f}"
         )
         
-        print('Patience = {patience:d}:{iteration:d} Residual = {Rk:.4f} | alpha = {alpha_k:.4f} beta = {beta_k:.4f} delta = {delta_k:.4f}"
+        print('Patience = {patience:d}:{iteration:d} Residual = {Rk:.4f} alpha = {alpha_k:.4f} beta = {beta_k:.4f} delta = {delta_k:.4f}')
               
         if Rk < Rk_best:  # has improvement
             # Record best
