ee33d8ffa8046674e43de6074d13cfe6acd39525
KalibrateBlockchain <74208473+KalibrateBlockchain@users.noreply.github.com>
2021-05-13 16:42:29 -0400

    Update ode_solver.py

--- solvers/ode_solvers/ode_solver.py
+++ solvers/ode_solvers/ode_solver.py
@@ -56,3 +56,54 @@ def ode_solver(
         sol.append([r.t, *list(r.y)])
 
     return np.array(sol)  # (t, [p, dp]) tangent bundle
+
+
+def ode_solver_1(
+    model: Callable,
+    model_jacobian: Callable,
+    model_params: List[float],
+    init_state: List[float],
+    init_t: float,
+    solver: str = "lsoda",
+    ixpr: int = 1,
+    dt: float = 0.1,
+    tmax: float = 1000,
+) -> np.ndarray:
+    """ ODE solver.
+    Args:
+        model: Callable
+            ODE model dy = f(t, y).
+        model_jacobian: Callable
+            Jacobian of ODE model.
+        model_params: List[float]
+            Model parameters.
+        init_state: List[float]
+            Initial model state.
+        init_t: float
+            Initial simulation time.
+        solver: str
+            Solver name. Options: vode, dopri5, dop853, lsoda; depends on stiffness and precision.
+        ixpr: int
+            Whether to generate extra printing at method switches.
+        dt: float
+            Time step increment.
+        tmax: float
+            Maximum simulation time.
+    Returns:
+        sol: np.ndarray[float]
+            Solution [time, model states].
+    """
+    sol = []
+
+    r = ode(model, model_jacobian)
+
+    r.set_f_params(*model_params)
+    r.set_jac_params(*model_params)
+    r.set_initial_value(init_state, init_t)
+    r.set_integrator(solver, with_jacobian=True, ixpr=ixpr)
+
+    while r.successful() and r.t < tmax:
+        r.integrate(r.t + dt)
+        sol.append([r.t, *list(r.y)])
+
+    return np.array(sol)  # (t, [p, dp]) tangent bundle
