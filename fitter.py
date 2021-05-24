8165c27355a41382b0330f1344273976da0c9f83
KalibrateBlockchain <74208473+KalibrateBlockchain@users.noreply.github.com>
2021-05-24 07:17:36 -0400

    Update fitter.py

--- fitter.py
+++ fitter.py
@@ -136,7 +136,7 @@ def vfo_fitter(gl_audio, rwt_audio, s_rate, period, numberOfPeriods):
   
   return res
 
-def vfo_vocal_fold_estimator(glottal_flow,wav_samples,sample_rate,t_patience = 5, section = 1, verbose):
+def vfo_vocal_fold_estimator(glottal_flow,wav_samples,sample_rate,verbose,t_patience = 5, section = 1):
     """
     Inputs: wav_samples: audio wavfile
             glottal_flow: numpy array of glottal flow from IAIF
